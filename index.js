934b36141d96f6fb96a4469e3505d8be6f5d595a
rochafialvin <rochafi.dev@gmail.com>
2020-01-28 10:25:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// 1. Memunculkan teks di terminal
+console.log("My first code ")
+
+// 2. Variable
+// Untuk menyimpan sebuah value / data
+// Tidak dapat diawali dengan angka
+// Diawali huruf, underscore, dollar sign
+// Dapat mengandung , huruf, number, underscore
+// camelCase
+var _name = "John Wick" // string
+var firstName = "John"
+var lastName = "Wick"
+var greet24 = "Hello, i'am John Wick"
+var $sarcas = 'Masakannya "enak" kok'
+var bisa = 'Hello, i\'am iron man'
+
+// Data Type
+
+var month = "January" // string
+var dateString = "28" // string
+var date = 28 // integer (bilangan bulat)
+var pi = 3.14 // float (bilangan desimal)
+var single = true // boolean (true, false)
+
+// function typeof untuk mengetahui tipe data dari sebuah variable
+var result = typeof(pi)
+console.log(result)
+
+var nilai = "32"
+var value = 32
+console.log(typeof(nilai))
+console.log(typeof(value))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SHORTCUT
+// Membuka / menutup terminal : CTRL + J
+// Membersihkan terminal : CTRL + L
+// Membuat komentar : CTRL + /
