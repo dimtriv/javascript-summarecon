e95fcdbd75a6035ad374d986c9384677c44d1980
rochafialvin <rochafi.dev@gmail.com>
2020-01-28 11:40:12 +0700

    SHORCUT

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 1. Memunculkan teks di terminal
-console.log("My first code ")
+// console.log("My first code ")
 
 // 2. Variable
 // Untuk menyimpan sebuah value / data
@@ -7,41 +7,67 @@ console.log("My first code ")
 // Diawali huruf, underscore, dollar sign
 // Dapat mengandung , huruf, number, underscore
 // camelCase
-var _name = "John Wick" // string
-var firstName = "John"
-var lastName = "Wick"
-var greet24 = "Hello, i'am John Wick"
-var $sarcas = 'Masakannya "enak" kok'
-var bisa = 'Hello, i\'am iron man'
+// var _name = "John Wick" // string
+// var firstName = "John"
+// var lastName = "Wick"
+// var greet24 = "Hello, i'am John Wick"
+// var $sarcas = 'Masakannya "enak" kok'
+// var bisa = 'Hello, i\'am iron man'
 
 // Data Type
 
-var month = "January" // string
-var dateString = "28" // string
-var date = 28 // integer (bilangan bulat)
-var pi = 3.14 // float (bilangan desimal)
-var single = true // boolean (true, false)
+// var month = "January" // string
+// var dateString = "28" // string
+// var date = 28 // integer (bilangan bulat)
+// var pi = 3.14 // float (bilangan desimal)
+// var single = true // boolean (true, false)
 
 // function typeof untuk mengetahui tipe data dari sebuah variable
-var result = typeof(pi)
-console.log(result)
+// var result = typeof(pi)
+// console.log(result)
 
-var nilai = "32"
-var value = 32
-console.log(typeof(nilai))
-console.log(typeof(value))
+// var nilai = "32"
+// var value = 32
+// console.log(typeof(nilai))
+// console.log(typeof(value))
+
+// Menampilkan teks
+// alert('Hello dude')
+
+// Menerima input dari user
+// var name = prompt('You name : ')
+// alert("Hello, my name is " + name)
+// console.log("Halo, watashi wa " + name + " desu")
+
+// backslash n (\n) digunakan untuk membuat baris baru (new line)
+// console.log(
+//     "This is my first line" + "\n\n\n" +
+//     "This is my second line"
+// )
+
+// Artimatik
+var numberOne = 10
+var numberTwo = 5
+
+var result = numberOne + numberTwo
+console.log(numberOne + " + " + numberTwo + " = " + result)
+
+result = numberOne - numberTwo
+console.log(numberOne + " - " + numberTwo + " = " + result)
+
+result = numberOne * numberTwo
+console.log(numberOne + " * " + numberTwo + " = " + result)
+
+result = numberOne / numberTwo
+console.log(numberOne + " / " + numberTwo + " = " + result)
+
+result = numberOne % numberTwo
+console.log(numberOne + " % " + numberTwo + " = " + result)
 
 
-
-
-
-
-
-
-
-
-
-
+console.log('First line')
+console.log('Second line')
+console.log('Third line')
 
 
 
@@ -50,6 +76,10 @@ console.log(typeof(value))
 
 
 // SHORTCUT
+// Ubah bersamaan : DRAG + CTRL + D
+// Kursor banyak : ALT + LEFT CLICK
+// Ubah posisi : ALT + ARROW
+// Duplicate code : SHIFT + ALT + ARROW BAWAH
 // Membuka / menutup terminal : CTRL + J
 // Membersihkan terminal : CTRL + L
 // Membuat komentar : CTRL + /
