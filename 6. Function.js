c12fe28c5cb8ba12d75487350ae562a5d3b6d22c
rochafialvin <rochafi.dev@gmail.com>
2020-02-06 11:06:18 +0700

    Sort-array-of-array

--- 6. Function.js
+++ 6. Function.js
@@ -22,24 +22,24 @@
 // greet('Dony')
 
 // 3 input, no output
-function intro(name, age, job){
-    console.log(
-        `Nama : ${name}\n` +
-        `Umur : ${age}\n` +
-        `Pekerjaan : ${job}`
-    )
-}
+// function intro(name, age, job){
+//     console.log(
+//         `Nama : ${name}\n` +
+//         `Umur : ${age}\n` +
+//         `Pekerjaan : ${job}`
+//     )
+// }
 
 // intro('Tony', 43, 'Engineer')
 // intro('John', 43, 'Hunter')
 
 // no input, 1 output
 // kode setelah return tidak di baca
-function grav(){
-    console.log('Sebelum return')
-    return 9.8
-    console.log('Setelah return')
-}
+// function grav(){
+//     console.log('Sebelum return')
+//     return 9.8
+//     console.log('Setelah return')
+// }
 
 // var result = grav()
 // console.group(`Satuan gravitasi : ${result}`)
@@ -48,9 +48,9 @@ function grav(){
 // Variable x dan y merupakan variable 'local'
 // yang artinya hanya dapat diakses di dalam function sum
 // jika mencoba mengakses diluar function sum akan error
-function sum(x, y){
-    return x +y 
-}
+// function sum(x, y){
+//     return x +y 
+// }
 
 
 // Ini akan error karena tidak bisa mengakses variable di dalam function sum.
@@ -132,27 +132,70 @@ function sum(x, y){
 // return nol : tidak bertukar posisi
 // return positif : posisi 'b' ada didepan 'a'
 
-numArr = [40, 23, 1, 100, 2, 230]
+// numArr = [40, 23, 1, 100, 2, 230]
 
 // ascending, kecil ke besar
-function asc(a, b){
-    return a - b
-}
+// function asc(a, b){
+//     return a - b
+// }
 
 //  descending, besar ke kecil
-function dsc(a, b){
-    return b - a
+// function dsc(a, b){
+//     return b - a
+// }
+
+// numArr.sort(dsc)
+
+// console.log(numArr)
+
+var persons = [
+    ['John', 43, 'Assasin'],
+    ['Tony', 44, 'Engineer'],
+    ['Baby', 21, 'Driver'],
+    ['Justin', 29, 'Singer']
+]
+
+// urutkan berdasarkan nama
+function byName(a, b){
+    // a = ['Tony', 44, 'Engineer']
+    // b = ['Baby', 43, 'Assasin']
+    if(a[0] < b[0]){
+        return -1
+
+    } else if(a[0] > b[0]){
+        return 1
+
+    } else {
+        return 0
+    }
 }
 
-numArr.sort(dsc)
+// urutkan berdasarkan umur  
+function byAge(a, b){
+    return a[1] - b[1]
+}                                                                                                                
 
-console.log(numArr)
+// urutkan berdasarkan pekerjaan
+function byJob(a, b){
+    // a = ['Tony', 44, 'Engineer']
+    // b = ['Baby', 43, 'Assasin']
+    if(a[2] < b[2]){
+        return -1
 
+    } else if(a[2] > b[2]){
+        return 1
 
+    } else {
+        return 0
+    }
+}
 
+persons.sort(byJob)
+console.log(persons)
 
-
-
+// Petunjuk
+// Untuk nama dan pekerjaan menggunakan perbandingan bias ( > / < / ==)
+// Untuk umur pakai asc
 
 
 
