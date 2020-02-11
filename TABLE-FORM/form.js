18ad4f659d8f9ed7e3ad8082a4d82cf28225db36
rochafialvin <rochafi.dev@gmail.com>
2020-02-11 11:15:42 +0700

    get value

---
+++ TABLE-FORM/form.js
@@ -0,0 +1,32 @@
+function submit() {
+    // document.getElementById("name") = inputObject
+    // document.getElementById("name").value = "John Wick"
+
+    // getElementById : return 1 object
+    // getElementsByName : return array
+
+    // querySelector / querySelectorAll : mencari element menggunakan query css
+    // querySelector : return 1 object
+    // querySelectorAll : return array
+
+    // input[name] : tag input yang memiliki attribute 'name'
+    // input[name=gender] : tag input yang memiliki attribute 'name' dg value 'gender'
+    // input[name=gender]:checked : tag input yang memiliki attribute 'name' dg value 'gender' dan di ceklis
+    var name = document.getElementById("name").value
+    var age = parseInt(document.getElementById("age").value)
+    var gender = document.querySelector("input[name=gender]:checked").value
+    console.log(name);
+    console.log(age);
+    console.log(gender);
+    
+    
+}
+
+// var genderArr = document.getElementsByName("gender")
+    // var gender
+
+    // for(var i = 0; i < genderArr.length; i++){
+    //     if(genderArr[i].checked == true){
+    //         gender = genderArr[i].value
+    //     }
+    // }
